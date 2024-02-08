b9851ef3694504569c0d629956a11cbb9674836a
MuhammetCanLikya <158784308+mySmurfChar@users.noreply.github.com>
2024-02-08 22:42:22 +0300

    first commit

---
+++ patikametotlar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata;
+
+namespace patikametotlar
+{
+    internal class Program
+    {
+        public static int topla(int a, int b)
+        {
+            //komutlar;
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(topla(4, 12));
+
+        }
+    }
+}
